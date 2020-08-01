0e6a734f0f3e058607cf58a2d3668f20a0da54e3
MicaelRiboura <53049385+MicaelRiboura@users.noreply.github.com>
2020-08-01 00:10:40 -0300

    Updates

---
+++ precache-manifest.71fb5996784ce2fc67b5fd78e0af4234.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557c8a3fd4563407dceb32bfcbfb3264",
+    "url": "/Filosoflix/index.html"
+  },
+  {
+    "revision": "2a36a872a0b6d70b8f5c",
+    "url": "/Filosoflix/static/css/main.d9bec4b3.chunk.css"
+  },
+  {
+    "revision": "b517b2ce67532c8b9d60",
+    "url": "/Filosoflix/static/js/2.fa3cd7be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Filosoflix/static/js/2.fa3cd7be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a36a872a0b6d70b8f5c",
+    "url": "/Filosoflix/static/js/main.f7b2faaa.chunk.js"
+  },
+  {
+    "revision": "82649b5ea5c8500d8557",
+    "url": "/Filosoflix/static/js/runtime-main.a5730be5.js"
+  },
+  {
+    "revision": "c058eb5675a3be0622c6062c53a33d06",
+    "url": "/Filosoflix/static/media/banner-fundo.c058eb56.bmp"
+  },
+  {
+    "revision": "f098c6b14b1482a88d9099210322e8d2",
+    "url": "/Filosoflix/static/media/logo.f098c6b1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9525cbaa84e1eaea96cbefcd1bb8cb08.js"
+  "/Filosoflix/precache-manifest.71fb5996784ce2fc67b5fd78e0af4234.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Filosoflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
